1d06d200e6835429877e0d8ce86ed96065661f50
Filipe Cayres <contato.fcayres@gmail.com>
2022-09-11 23:01:36 -0300

    calculadora js ap

---
+++ Calculadora/assets/script.js
@@ -0,0 +1,23 @@
+function insert(number) {
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + number;
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else{
+        document.getElementById('resultado').innerHTML = '';
+    }
+}
