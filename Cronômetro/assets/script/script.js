439eb7c9121abad979cdd42b7219fedb564d53e9
Filipe Cayres <contato.fcayres@gmail.com>
2022-09-24 21:56:24 -0300

    timer att

---
+++ Cronômetro/assets/script/script.js
@@ -0,0 +1,40 @@
+var sec=0
+var min=0
+var hr=0
+var interval
+
+function twoDigits(digit){
+if(digit<10){
+    return('0'+digit)
+}else{
+    return(digit)
+}
+}
+
+function start(){    
+    interval = setInterval(watch, 1000)
+}
+
+function pause(){
+    clearInterval(interval)
+}
+
+function stoP(){
+    clearInterval(interval)
+    sec=0
+    min=0
+    document.getElementById('time').innerText='00:00:00'
+}
+
+function watch(){
+    sec++
+    if(sec==60){
+        min++
+        sec=0
+        if(min==60){
+            min=0
+            hr++
+        }
+    }
+    document.getElementById('time').innerHTML=twoDigits(hr) +':' + twoDigits(min) + ':' + twoDigits(sec)
+}
