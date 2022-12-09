8668f4231270a461fcaf211fd17fb50808357be1
Filipe Cayres <contato.fcayres@gmail.com>
2022-12-08 23:22:47 -0300

    imc final.1

--- Projetos-basicos/IMC/assets/imc.js
+++ Projetos-basicos/IMC/assets/imc.js
@@ -1,7 +1,5 @@
 let calcular = document.getElementById("calcular");
 
-
-
 function imc() {
     let nome = document.getElementById("nome").value;
     let altura = document.getElementById("altura").value;
@@ -10,9 +8,25 @@ function imc() {
 
     if(nome !== '' && altura !== '' && peso !== ''){
 
-        const valorIMC = (peso /(altura * altura));
+        const valorIMC = (peso /(altura * altura)).toFixed(1);
+        let classificacao = '';
 
-        resultado.textContent = valorIMC;
+        if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        } else if(valorIMC < 25){
+            classificacao = 'com o peso ideal. Parabéns!!!';
+        } else if(valorIMC < 30){
+            classificacao = 'levemente acima do peso. Tenha cuidado!';
+        } else if(valorIMC < 35){
+            classificacao = 'com obesidade grau I!';
+        } else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II!';
+        } else {
+            classificacao = 'com obesidade grau III. Cuidado!!!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
     }else {
         resultado.textContent = 'Por favor, preencha todos os campos para calcular o IMC!!!';
     }
