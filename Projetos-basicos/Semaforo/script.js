2818379f63327bf5f04ac70c9ee91aa639bc89fe
Filipe Cayres <contato.fcayres@gmail.com>
2022-12-14 21:21:16 -0300

    js init

---
+++ Projetos-basicos/Semaforo/script.js
@@ -0,0 +1,8 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+
+const trafficLight = () => {
+    
+}
+
+buttons.addEventListener('click', trafficLight);
