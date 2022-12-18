c90c15c524054421684983b099f2085dba52e08a
Filipe Cayres <contato.fcayres@gmail.com>
2022-12-17 21:42:26 -0300

    Semaforo lights updt

--- Projetos-basicos/Semaforo/script.js
+++ Projetos-basicos/Semaforo/script.js
@@ -1,8 +1,14 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 
-const trafficLight = () => {
-    
+const trafficLight = (event) => {
+    turnOn[event.target.id]()
+}
+
+const turnOn = {
+    'red': () => img.src = './images/vermelho.png',
+    'yellow': () => img.src = './images/amarelo.png',
+    'green': () => img.src = './images/verde.png'
 }
 
 buttons.addEventListener('click', trafficLight);
