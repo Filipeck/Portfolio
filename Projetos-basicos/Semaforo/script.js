8c5c51cc50e4c828f5d2c07156a6f480970ec7d3
Filipe Cayres <contato.fcayres@gmail.com>
2022-12-18 21:41:37 -0300

    project finalversion1

--- Projetos-basicos/Semaforo/script.js
+++ Projetos-basicos/Semaforo/script.js
@@ -1,14 +1,31 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalID = null;
 
-const trafficLight = (event) => {
-    turnOn[event.target.id]()
+const trafficLight = (event) => {    
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changecolor = () => {
+    const colors = ['red', 'yellow', 'green']
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic = () => {
+    clearInterval ( intervalID);
 }
 
 const turnOn = {
     'red': () => img.src = './images/vermelho.png',
     'yellow': () => img.src = './images/amarelo.png',
-    'green': () => img.src = './images/verde.png'
+    'green': () => img.src = './images/verde.png',
+    'automatic': () => intervalID = setInterval(changecolor, 1000)
 }
 
 buttons.addEventListener('click', trafficLight);
